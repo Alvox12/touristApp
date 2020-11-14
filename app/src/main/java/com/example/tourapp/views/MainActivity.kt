0d5f1330f263d2986e31180bd27ebeca435caf99
alvaro <urda.alvaro@gmail.com>
2020-11-14 08:42:57 +0100

    Activamos opcion cerrar sesion

--- app/src/main/java/com/example/tourapp/views/MainActivity.kt
+++ app/src/main/java/com/example/tourapp/views/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.tourapp.views
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import com.example.tourapp.R
+import com.example.tourapp.commons.Constants
+import com.example.tourapp.commons.SharedPreferencesManager
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_logout.view.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private  var mFirebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,7 +41,47 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
+        when (item.itemId) {
+            R.id.nav_logout -> showDialogLogout(item)
+        }
+        return true
+    }
+
+    /**
+     * Método para desconectar la conexión con firebase
+     */
+    private fun logout(){
+        SharedPreferencesManager.setSomeBooleanValues(Constants.SAVELOGIN, false)
+        mFirebaseAuth?.signOut()
+        Log.v("FIREBASE_LOGOUT", "LOGOUT")
+
+    }
+
+
+    /**
+     * Funcion que infla una modal para desloguear al usuario de la app
+     */
+    private fun showDialogLogout(itemId: MenuItem) {
+
+        val dialogBuilder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_logout, null)
+        val b = dialogBuilder.setView(dialogView).create()
+
+        dialogView.btnCerrar.setOnClickListener{
+            itemId.isChecked = false
+            b.dismiss()
+        }
+
+        dialogView.btnAceptar.setOnClickListener{
+            logout()
+            startActivity(Intent(this, LoginActivity::class.java))
+            //SharedPreferencesManager.setSomeStringValues(Constants.PASSWORD, "") No quito la pass de shared preferences
+            b.dismiss()
+            finish()
+        }
+
+        b.setCancelable(false)
+        b.show()
     }
 
 }
